1db67bc3978d30c56ad98d68141e63a2301b5e29
Susan Boyd <sueboyd922@gmail.com>
2022-07-10 16:27:11 -0400

    Set up GraphQL

---
+++ app/graphql/types/game_activity_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class GameActivityType < Types::BaseObject
+    field :id, ID, null: false
+    field :activity_id, Integer, null: true
+    field :game_id, Integer, null: true
+    field :completed, Boolean, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
