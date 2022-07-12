0e4eb8cc74e2888ffaa8e7259e752b2ac2014c0d
Susan Boyd <sueboyd922@gmail.com>
2022-07-12 08:19:43 -0400

    Create Game Mutation started

---
+++ app/graphql/mutations/create_game.rb
@@ -0,0 +1,12 @@
+class Mutations::CreateGame < Mutations::BaseMutation
+  argument :level, String, required: true
+  argument :user_id, Integer, required: true
+
+  field :game Types::GameType, null: false
+  field :errors, [String], null: false
+
+  def resolve(level:, user_id:)
+    game = Game.new(level: level, user_id: user_id)
+    require "pry"; binding.pry
+  end
+end
