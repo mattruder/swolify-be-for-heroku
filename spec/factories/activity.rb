298685d5fe6ed243054e60bc8e7f8553b721e274
Susan Boyd <sueboyd922@gmail.com>
2022-07-12 08:43:17 -0400

    Set up test mutation

--- spec/factories/activity.rb
+++ spec/factories/activity.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :activity do
     name { Faker::Verb.base }
     category { Faker::Number.between(from: 0, to: 3) }
-    duration { Faker::Lorem.words(number: 3) }
+    duration { Faker::Lorem.sentence(word_count: 3) }
     video { "video.com" }
     description { Faker::TvShows::NewGirl.quote }
   end
---
+++ spec/graphql/mutations/create_game_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'create game mutation' do
+  let!(:user) { User.create!(name: "Susan", email: "susan@email.com") }
+  let!(:upper_body_activities) { create_list(:activity, 16, category: 0) }
+  let!(:lower_body_activities) { create_list(:activity, 16, category: 1) }
+  let!(:core_activities) { create_list(:activity, 16, category: 2) }
+  let!(:cardio_activities) { create_list(:activity, 16, category: 3) }
+
+  describe 'create a game' do
+    it 'creates a game' do
+      level = "easy"
+      mutation = <<~GQL
+      mutation {
+        createGame {
+          user_id: #{user.id}
+          level: #{level}
+        }
+      }
+      GQL
+
+      result = SwolifyBeSchema.execute(mutation)
+      require "pry"; binding.pry
+    end
+  end
+end
