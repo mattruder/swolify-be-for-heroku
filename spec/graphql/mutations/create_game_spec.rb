b603d6d2859edb443bc699b6f063eb3cfd957f88
Susan Boyd <sueboyd922@gmail.com>
2022-07-12 17:36:10 -0400

    Test for easy vs hard games in CreateGame mutation

--- spec/graphql/mutations/create_game_spec.rb
+++ spec/graphql/mutations/create_game_spec.rb
@@ -1,55 +1,85 @@
 require 'rails_helper'
-module Mutations
-  module Games
 
-RSpec.describe CreateGame, type: :request do
+describe 'createGame mutation', type: :request do
   let!(:user) { User.create!(name: "Susan", email: "susan@email.com") }
   let!(:upper_body_activities) { create_list(:activity, 16, category: 0) }
   let!(:lower_body_activities) { create_list(:activity, 16, category: 1) }
   let!(:core_activities) { create_list(:activity, 16, category: 2) }
   let!(:cardio_activities) { create_list(:activity, 16, category: 3) }
 
-  let!(:level) { "easy" }
-  let!(:categories) {["core", "upper body"]}
-
   describe '.resolve' do
-    it 'creates a game' do
-      # level = "easy"
-      # categories = ["core", "upper body"]
-      # input = {
-      #   userId: user.id,
-      #   level: level,
-      #   categories: categories
-      # }
-
-      post "/graphql", params: {query: query}
-      require "pry"; binding.pry
-      json = JSON.parse(response.body, symbolize_names: true)
-
-      # require "pry"; binding.pry
-      # result = SwolifyBeSchema.execute(mutation)
-      # require "pry"; binding.pry
-    end
-
-    def query
-      <<~GQL
+    it 'creates an easy game with 9 unique activities' do
+      level = "easy"
+      categories = ["core", "upper body"]
+      query = <<~GQL
       mutation {
-        createGame(input: {
-          userId: "#{user.id}",
+        createGame(input: {params: {
+          userId: #{user.id},
           level: "#{level}",
-          categories: "#{categories}"
-          })
+          categories: #{categories}
+          }})
         {
-          game {
-            level
+        game {
+          userId
+          gameActivities{
+            id
+            activity {
+              name
+              description
+              duration
+              video
+              }
+            }
           }
-        errors
         }
       }
       GQL
+
+      result = SwolifyBeSchema.execute(query)
+
+      expect(result.dig("data", "createGame", "game", "userId")).to eq(user.id)
+      expect(result.dig("data", "createGame", "game", "gameActivities").count).to eq(9)
+
+      activity_names = result.dig("data", "createGame", "game", "gameActivities").map {|act| act.dig("activity", "name")}
+
+      expect(activity_names.uniq).to eq(activity_names)
     end
 
+    it 'creates a hard game with 16 unique activities' do
+      level = "hard"
+      categories = ["cardio", "lower body"]
+      query = <<~GQL
+      mutation {
+        createGame(input: {params: {
+          userId: #{user.id},
+          level: "#{level}",
+          categories: #{categories}
+          }})
+        {
+        game {
+          userId
+          gameActivities{
+            id
+            activity {
+              name
+              description
+              duration
+              video
+              }
+            }
+          }
+        }
+      }
+      GQL
+
+      result = SwolifyBeSchema.execute(query)
+
+      expect(result.dig("data", "createGame", "game", "userId")).to eq(user.id)
+      expect(result.dig("data", "createGame", "game", "gameActivities").count).to eq(16)
+
+      activity_names = result.dig("data", "createGame", "game", "gameActivities").map {|act| act.dig("activity", "name")}
+
+      expect(activity_names.uniq).to eq(activity_names)
+    end
   end
-end
-end
 end
