682918f4ed96eb8b860ef62e677c9fe3ed71f455
Susan Boyd <sueboyd922@gmail.com>
2022-07-18 16:01:35 -0400

    Add success field to ModifyGame mutation

--- app/graphql/mutations/modify_game.rb
+++ app/graphql/mutations/modify_game.rb
@@ -1,14 +1,16 @@
 class Mutations::ModifyGame < Mutations::BaseMutation
   argument :params, Types::Input::GameInputType, required: true
 
-  field :game, Types::GameType, null: false
+  field :game, Types::GameType, null: true
+  field :success, Boolean, null: false
 
   def resolve(params:)
     game = Game.find(params[:id])
     if game.update(win: params[:win])
       game.complete_game_activities(params[:activities])
       {
-        game: game
+        game: game,
+        success: true
       }
     end
   rescue ActiveRecord::RecordNotFound => _e
