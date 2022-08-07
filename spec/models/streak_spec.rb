7ca88fa91203f563e3c7817d1933e94d6a2ed23f
Susan Boyd <sueboyd922@gmail.com>
2022-08-07 11:06:21 -0400

    Add streak model tests for instance methods

--- spec/models/streak_spec.rb
+++ spec/models/streak_spec.rb
@@ -16,4 +16,25 @@ RSpec.describe Streak, type: :model do
       expect(streak.days_in_a_row).to eq(1)
     end
   end
+
+  describe 'instance methods' do
+    let!(:user) { User.create!(name: "Tony Soprano", email: "who_ate_all_the_gabagool@sopranos.net") }
+
+    it '#increase_by_one' do
+      streak = Streak.create!(created_at: Date.yesterday, updated_at: Date.yesterday, user_id: user.id)
+      streak.increase_by_one
+      expect(streak.days_in_a_row).to eq(2)
+    end
+
+    it '#not_updated_yet_today' do
+      yesterdays_streak = Streak.create!(created_at: Date.yesterday, updated_at: Date.yesterday, user_id: user.id)
+      todays_streak = Streak.create!(user_id: user.id)
+
+      expect(yesterdays_streak.not_updated_yet_today).to eq(true)
+      expect(todays_streak.not_updated_yet_today).to eq(false)
+
+      yesterdays_streak.increase_by_one
+      expect(yesterdays_streak.not_updated_yet_today).to eq(false)
+    end
+  end
 end
